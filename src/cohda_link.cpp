8b1716aac5c3c77f32f9188f2115a1248ffd1520
Tiago Garcia <tiago.rgarcia@av.it.pt>
2025-07-21 14:48:42 +0100

    Fix cohda ublox implementation
    Signed-off-by: Tiago Garcia <tiago.rgarcia@av.it.pt>

--- src/cohda_link.cpp
+++ src/cohda_link.cpp
@@ -4,18 +4,23 @@
 #include <vanetza/access/data_request.hpp>
 #include <vanetza/access/g5_link_layer.hpp>
 #include <vanetza/common/byte_buffer.hpp>
+#include <vanetza/common/byte_order.hpp>
 #include <vanetza/common/clock.hpp>
 #include <vanetza/common/serialization_buffer.hpp>
 #include <vanetza/dcc/mapping.hpp>
 #include <vanetza/net/chunk_packet.hpp>
 #include <vanetza/net/cohesive_packet.hpp>
 #include <vanetza/net/ethernet_header.hpp>
+#include <vanetza/net/osi_layer.hpp>
 
 #include <boost/optional.hpp>
 #include <boost/optional/optional.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <boost/range/iterator_range_core.hpp>
 
 extern "C" {
 #include <pthread.h>
+#include <linux/cohda/llc/llc-api.h>
 }
 
 #include <cassert>
@@ -24,37 +29,70 @@ extern "C" {
 
 using namespace vanetza;
 
-boost::optional<EthernetHeader> CohdaLink::parse_ethernet_header(vanetza::CohesivePacket & packet) const {
-  packet.set_boundary(OsiLayer::Physical, 0);
-  if (packet.size(OsiLayer::Link) < EthernetHeader::length_bytes) {
-    std::cerr << "Router dropped invalid packet (too short for Ethernet header)\n";
-  } else {
-    packet.set_boundary(OsiLayer::Link, EthernetHeader::length_bytes);
-    auto link_range = packet[OsiLayer::Link];
-    return decode_ethernet_header(link_range.begin(), link_range.end());
-  }
-  return boost::none;
+boost::optional<EthernetHeader> CohdaLink::parse_ethernet_header(vanetza::CohesivePacket & packet) const
+{
+    const std::size_t cohda_header_size = 2;
+    if (packet.size(OsiLayer::Physical) < cohda_header_size)
+        return boost::none;
+
+    const std::size_t mac_header_size = (packet[OsiLayer::Physical][0] & 0x80) != 0 ? 24 : 22;
+    const std::size_t llc_snap_header_size = 8;
+    const std::size_t link_layer_size = mac_header_size + llc_snap_header_size;
+
+    const size_t dst_addr_offset = 2;
+    const size_t src_addr_offset = 8;
+    const size_t ether_type_offset = mac_header_size + 6;
+
+    if (packet.size(OsiLayer::Physical) < cohda_header_size + link_layer_size)
+        return boost::none;
+
+    packet.set_boundary(OsiLayer::Physical, cohda_header_size);
+    packet.set_boundary(OsiLayer::Link, link_layer_size);
+    packet.set_boundary(OsiLayer::Network, packet.size());
+
+    const auto& link_layer_bytes = packet[OsiLayer::Link];
+    EthernetHeader eth;
+
+    deserialize_from_range(eth.destination, boost::make_iterator_range(
+        link_layer_bytes.begin() + dst_addr_offset,
+        link_layer_bytes.begin() + dst_addr_offset + MacAddress::length_bytes
+    ));
+
+    deserialize_from_range(eth.source, boost::make_iterator_range(
+        link_layer_bytes.begin() + src_addr_offset,
+        link_layer_bytes.begin() + src_addr_offset + MacAddress::length_bytes
+    ));
+
+    deserialize_from_range(eth.type, boost::make_iterator_range(
+        link_layer_bytes.begin() + ether_type_offset,
+        link_layer_bytes.begin() + ether_type_offset + 2
+    ));
+
+    return eth;
 }
 
 void CohdaLink::rx_callback(it2s_ublox_t *ublox, void* user_data, uint8_t* buf, uint16_t packet_len){
-  auto* self = static_cast<CohdaLink*>(ublox->userData);
+  auto* self = static_cast<CohdaLink*>(user_data);
 
-  // debug 
-  char *rx_hex = (char*)malloc(packet_len*2+2);
-  char *buf_ptr = rx_hex;
-  uint8_t *pkt_ptr = buf;
-  for (int i = 0; i < packet_len; i++)
-    buf_ptr += sprintf(buf_ptr, "%02x", pkt_ptr[i]);
-  printf("[ublox]<- received packet |size:%dB data:%.*s", packet_len, packet_len*2, rx_hex);
-  free(rx_hex);
+  // For debugging, print the raw received hex string.
+  // char *rx_hex = (char*)malloc(packet_len*2+2);
+  // char *buf_ptr = rx_hex;
+  // uint8_t *pkt_ptr = buf;
+  // for (int i = 0; i < packet_len; i++)
+  //   buf_ptr += sprintf(buf_ptr, "%02x", pkt_ptr[i]);
+  // printf("[ublox]<- received packet |size:%dB data:%.*s\n", packet_len, packet_len*2, rx_hex);
+  // free(rx_hex);
 
-  /* UNCOMMENT AFTER TESTING RX
-  // parse into vanetza packet and send upwards
   ByteBuffer buffer(buf, buf + packet_len);
   CohesivePacket packet(std::move(buffer), OsiLayer::Physical);
   boost::optional<EthernetHeader> eth = self->parse_ethernet_header(packet);
-  self->indicate_to_router_(std::move(packet), *eth);
-  */
+
+  if (eth) {
+    self->indicate_to_router_(std::move(packet), *eth);
+  } else {
+    // Optional: Handle the case where a packet could not be parsed.
+    fprintf(stderr, "[ublox]<- Packet parsing failed.\n");
+  }
 }
 
 CohdaLink::CohdaLink() {
